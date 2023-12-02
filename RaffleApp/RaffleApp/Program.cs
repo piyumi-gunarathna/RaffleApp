2505362c81f1594740223ae2985dc7f956fbfb18
Piyumi Gunarathna <piyumi.gunarathna@gmail.com>
2023-12-02 22:29:07 +0800

    Added app start logic and DI

--- RaffleApp/RaffleApp/Program.cs
+++ RaffleApp/RaffleApp/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using RaffleApp;
 
+IServiceCollection services = new ServiceCollection();
+services.AddSingleton<ITicketNumbersGenerator, TicketNumbersGenerator>();
+services.AddSingleton<IRaffle, Raffle>();
+services.AddSingleton< RaffleConsoleManager>();
+
+IServiceProvider serviceProvider = services.BuildServiceProvider();
+RaffleConsoleManager raffleConsoleManager = serviceProvider.GetRequiredService<RaffleConsoleManager>();
+
+
+while (true)
+{
+    try
+    {
+        raffleConsoleManager.Run();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(Constants.UNEPECTED_ERROR + ex.Message);
+    }
+}
